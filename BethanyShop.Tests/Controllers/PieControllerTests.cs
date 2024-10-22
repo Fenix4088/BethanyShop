9835f7039beceb323b7ddaf787982f909c5a8612
Yehor Pliasov <fenix@yehors-macbook-air.home>
2024-10-22 15:24:27 +0200

    xUnit

--- BethanyShop.Tests/Controllers/PieControllerTests.cs
+++ BethanyShop.Tests/Controllers/PieControllerTests.cs
@@ -1,6 +1,26 @@
+using BethaniShop.API.Controllers;
+using BethaniShop.API.ViewModels;
+using BethanyShop.Tests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+
 namespace BethanyShop.Tests.Controllers;
 
 public class PieControllerTests
 {
-    
+    [Fact]
+    public void List_EmptyCategory_ReturnsAllPies()
+    {
+        //arrange
+        var mockedPieRepository = RepositoryMocks.GetPieRepository();
+        var mockedCategoryRepository = RepositoryMocks.GetCategoryRepository();
+        var pieController = new PieController(mockedPieRepository.Object, mockedCategoryRepository.Object);
+        
+        //act
+        var result = pieController.List("");
+        
+        //assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+        Assert.Equal(10, pieListViewModel.Pies.Count());
+    }
 }
