868dea674e040aa72c397f0c69b9d0d2984d05d3
Yehor Pliasov <fenix@yehors-macbook-air.home>
2024-10-23 16:45:35 +0200

    uthorisation added

--- BethanyShop.Tests/obj/Debug/net8.0/BethanyShop.Tests.AssemblyInfo.cs
+++ BethanyShop.Tests/obj/Debug/net8.0/BethanyShop.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BethanyShop.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d382ecac78387aa4e5e642cda290e6e8ce5b353")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c542fc150082579a2ad1ab48a9db96aadf1e757")]
 [assembly: System.Reflection.AssemblyProductAttribute("BethanyShop.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BethanyShop.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
